eea77b152fb7c3f0d2d6730e71f01d7b7debdaea
Mark Vismonte <mark.vismonte@gmail.com>
2012-10-26 23:07:56 -0700

    Fixed stuff

--- part2/load.sql
+++ part2/load.sql
@@ -3,13 +3,13 @@ LOAD DATA LOCAL INFILE 'items.csv' INTO TABLE Item
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
 
 # Loads data from <insertfilename> file into the table Users.
-LOAD DATA LOCAL INFILE '<users.csv>' INTO TABLE User
+LOAD DATA LOCAL INFILE 'users.csv' INTO TABLE User
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
 
 # Loads data from <insertfilename> file into the table Bids.
-LOAD DATA LOCAL INFILE 'bids.csv' INTO TABLE Bid (ItemID, UserID, Time, Amount)
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA LOCAL INFILE 'bids.csv' INTO TABLE Bid FIELDS 
+TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' (ItemID, UserID, Time, Amount);
 
 # Loads data from <insertfilename> file into the table Categories.
-LOAD DATA LOCAL INFILE '<itemCategories.csv>' INTO TABLE ItemCategory
+LOAD DATA LOCAL INFILE 'itemCategories.csv' INTO TABLE ItemCategory
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
