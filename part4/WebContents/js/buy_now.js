5377792d7e866bd9493a4b9fe408515dca91bf07
Mark Vismonte <mark.vismonte@gmail.com>
2012-12-02 20:41:52 -0800

    Fixed whatever error Logan said we were having

--- part4/WebContents/js/buy_now.js
+++ part4/WebContents/js/buy_now.js
@@ -1,4 +1,9 @@
 
+// Hack around making console.log work when it doesn't exist.
+if (!console.log) {
+  console.log = function() {};
+}
+
 BuyNowViewModel = function () {
   this.id = $.cookie('item_id');
   this.name = $.cookie('item_name');
@@ -9,22 +14,33 @@ BuyNowViewModel = function () {
 
   var self = this;
   this.buy = function() {
-  // Make an AJAX call sending credit_card.
-  $.post('/eBay/buy_now', { 'credit_card': self.credit_card }, function(data) {
-    var obj = jQuery.parseJSON(data);
-    if (obj.valid) {
-      var currentTime = new Date()
-      var year = currentTime.getFullYear();
-      var month = currentTime.getMonth() + 1;
-      var day = currentTime.getDate();
-      var hour = currentTime.getHours();
-      var minute = currentTime.getMinutes();
-      var second = currentTime.getSeconds();
-      self.buy_time(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
-      self.is_buying_item(false);
-    }
-  });
-};
+    // Make an AJAX call sending credit_card.
+    var settings = {
+      'url': '/eBay/buy_now',
+      'data': {
+        'credit_card': self.credit_card
+      },
+      'type': 'POST',
+      'dataType': 'json'
+    };
+    settings['success'] = function(data) {
+      // var obj = jQuery.parseJSON(data);
+      console.log(data);
+      if (data.valid) {
+        var currentTime = new Date()
+        var year = currentTime.getFullYear();
+        var month = currentTime.getMonth() + 1;
+        var day = currentTime.getDate();
+        var hour = currentTime.getHours();
+        var minute = currentTime.getMinutes();
+        var second = currentTime.getSeconds();
+        self.buy_time(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
+        self.is_buying_item(false);
+      }
+    };
+    // Initiate the request.
+    $.ajax(settings);
+  };
 }
 
 
