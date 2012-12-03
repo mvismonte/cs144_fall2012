5e5932301648f8b2fe435dbc73bed59fc16a8c0b
Logan Chang <logiebear1214@gmail.com>
2012-12-02 18:56:54 -0800

    Finished POST and current timestamp.

--- part4/WebContents/js/buy_now.js
+++ part4/WebContents/js/buy_now.js
@@ -6,13 +6,28 @@ BuyNowViewModel = function () {
   this.is_buying_item = ko.observable(true);
   this.credit_card = ko.observable(null);
   this.buy_time = ko.observable(null);
+
+  var self = this;
+  this.buy = function() {
+  // Make an AJAX call sending credit_card.
+  $.post('/eBay/buy_now', { 'credit_card': self.credit_card }, function(data) {
+    var obj = jQuery.parseJSON(data);
+    if (obj.valid) {
+      var currentTime = new Date()
+      var year = currentTime.getFullYear();
+      var month = currentTime.getMonth() + 1;
+      var day = currentTime.getDate();
+      var hour = currentTime.getHours();
+      var minute = currentTime.getMinutes();
+      var second = currentTime.getSeconds();
+      self.buy_time(year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second);
+      self.is_buying_item(false);
+    }
+  });
+};
 }
 
-BuyNowViewModel.prototype.buy = function() {
-  // Make an AJAX call sending credit_card.
-  this.buy_time('Now');
-  this.is_buying_item(false);
-};
+
 
 var buyNowViewModel = new BuyNowViewModel();
 $(document).ready(function () {
--- part4/src/edu/ucla/cs/cs144/BuyServlet.java
+++ part4/src/edu/ucla/cs/cs144/BuyServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import edu.ucla.cs.cs144.Utilities;
 
 public class BuyServlet extends HttpServlet implements Servlet {
-       
+
     public BuyServlet() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -26,6 +26,24 @@ public class BuyServlet extends HttpServlet implements Servlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-
+      Valid obj = new Valid(true);
+      Gson gson = new Gson();
+      String json = gson.toJson(obj);
+      PrintWriter out = response.getWriter();
+      out.println(json);
+      out.close();
+      response.setContentType("application/json");
     }
+
+  private class Valid {
+    private boolean valid;
+
+    public Valid(boolean valid) {
+      this.valid = valid;
+    }
+  }
 }
+/*
+{
+  'valid': true
+}*/
